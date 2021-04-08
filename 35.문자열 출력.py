38cf65ac78687ab7fc035a3aa19dc11a1ac58a86
dofany <kimdohwan17@gmail.com>
2021-04-08 20:27:03 +0900

    35. 문자열 출력

--- 35.문자열 출력.py
+++ 35.문자열 출력.py
@@ -11,5 +11,5 @@ name1 = "김민수"
 age1 = 10
 name2 = "이철희"
 age2 = 13
-print("이름: {} 나이: {}".format(name1,age1))
-print("이름: {} 나이: {}".format(name2,age2))
+print("이름: %s 나이: %d"%(name1,age1))
+print("이름: %s 나이: %d"%(name2,age2))
