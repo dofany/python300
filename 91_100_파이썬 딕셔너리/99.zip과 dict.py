1c511b2ae73288a053174cfddd34b0bf33363107
dofany <kimdohwan17@gmail.com>
2021-04-09 01:08:53 +0900

    99. zip과 dict

---
+++ 91_100_파이썬 딕셔너리/99.zip과 dict.py
@@ -0,0 +1,13 @@
+# 아래 두 개의 튜플을 하나의 딕셔너리로 변환하라. keys를 키로, vals를 값으로 result 이름의 딕셔너리로 저장한다.
+#
+# keys = ("apple", "pear", "peach")
+# vals = (300, 250, 400)
+# 실행 예시:
+# >> print(result)
+# {'apple': 300, 'pear': 250, 'peach': 400}
+
+keys = ("apple", "pear", "peach")
+vals = (300, 250, 400)
+
+result = dict(zip(keys,vals))
+print(result)
