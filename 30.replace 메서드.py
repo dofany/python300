2a4b352ffc3b639ce193a124977dd5cc1f6074c5
dofany <kimdohwan17@gmail.com>
2021-04-08 20:09:55 +0900

    30. replace 메서드

---
+++ 30.replace 메서드.py
@@ -0,0 +1,11 @@
+# 아래 코드의 실행 결과를 예상해보세요.
+# >> string = 'abcd'
+# >> string.replace('b', 'B')
+# >> print(string)
+
+string = 'abcd'
+string.replace('b','B')
+print(string)
+print(string.replace('b','B'))
+
+print("바인딩 할 변수가 없어 변환이 되지 않는다. 문자열은 수정 불가하다")
