9fad57257b7573e91c971d922018709bcf22660f
dofany <kimdohwan17@gmail.com>
2021-04-08 19:30:08 +0900

    13. 문자열 출력

---
+++ 13.문자열 출력.py
@@ -0,0 +1,12 @@
+# 변수 s와 t에는 각각 문자열이 바인딩 되어있습니다.
+#
+# >> s = "hello"
+# >> t = "python"
+# 두 변수를 이용하여 아래와 같이 출력해보세요.
+#
+# 실행 예:
+# hello! python
+
+s = "hello"
+t = "python"
+print("{0}! {1}".format(s,t))
