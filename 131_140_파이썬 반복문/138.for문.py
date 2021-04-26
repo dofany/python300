4f452449d6a1ed948e1c37d33f9274504542c5c8
dofany <kimdohwan17@gmail.com>
2021-04-26 20:18:39 +0900

    138. for문

---
+++ 131_140_파이썬 반복문/138.for문.py
@@ -0,0 +1,13 @@
+# 다음 코드를 for문으로 작성하라.
+#
+# print(10)
+# print("-------")
+# print(20)
+# print("-------")
+# print(30)
+# print("-------")
+
+arr = 10,20,30
+for i in arr:
+    print(i)
+    print("-------")
