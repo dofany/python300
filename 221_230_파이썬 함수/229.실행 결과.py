2e42d7e83fe279a9ecbf0d4997b363064ab5d5a1
dofany <kimdohwan17@gmail.com>
2021-05-03 21:37:08 +0900

    229. 실행 결과

---
+++ 221_230_파이썬 함수/229.실행 결과.py
@@ -0,0 +1,13 @@
+# 아래 코드의 실행 결과를 예측하라.
+#
+# def my_print (a, b) :
+#     print("왼쪽:", a)
+#     print("오른쪽:", b)
+#
+# my_print(a=100, b=200)
+
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(a=100, b=200)
