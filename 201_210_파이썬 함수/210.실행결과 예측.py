bb99d1459f9c3d310a77af8cf089038eacb6167e
dofany <kimdohwan17@gmail.com>
2021-05-03 20:33:10 +0900

    210. 실행결과 예측

---
+++ 201_210_파이썬 함수/210.실행결과 예측.py
@@ -0,0 +1,30 @@
+# 아래 코드의 실행 결과를 예측하라.
+#
+# def message1():
+#     print("A")
+#
+# def message2():
+#     print("B")
+#
+# def message3():
+#     for i in range (3) :
+#         message2()
+#         print("C")
+#     message1()
+#
+# message3()
+
+
+def message1():
+    print("A")
+
+def message2():
+    print("B")
+
+def message3():
+    for i in range (3) :
+        message2()
+        print("C")
+    message1()
+
+message3()
