6a466b6aacafb7ba5a8368c9ba9c4671b2e30f41
dofany <kimdohwan17@gmail.com>
2021-05-03 21:36:20 +0900

    228. 연봉

---
+++ 221_230_파이썬 함수/228.연봉.py
@@ -0,0 +1,9 @@
+# 연봉을 입력받아 월급을 계산하는 calc_monthly_salary(annual_salary) 함수를 정의하라. 회사는 연봉을 12개월로 나누어 분할 지급하며, 이 때 1원 미만은 버림한다.
+#
+# calc_monthly_salary(12000000)
+# 1000000
+
+def calc_monthly_salary(annual_pay):
+    print(int(annual_pay / 12))
+
+calc_monthly_salary(12000000)
