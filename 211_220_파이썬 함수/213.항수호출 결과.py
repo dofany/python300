ea9b94ca43cb3d3ddff0cdc7a2b2c8beb5802322
dofany <kimdohwan17@gmail.com>
2021-05-03 20:36:45 +0900

    213. 함수호출 결과

---
+++ 211_220_파이썬 함수/213.항수호출 결과.py
@@ -0,0 +1,12 @@
+# 아래와 같은 에러가 발생하는 원인을 설명하라.
+#
+# def 함수(문자열) :
+#     print(문자열)
+# 함수()
+# TypeError: 함수() missing 1 required positional argument: '문자열'
+
+def 함수(문자열):
+    print(문자열)
+
+print("함수에 정의와 다르게 함수를 호출하고 있다.")
+
