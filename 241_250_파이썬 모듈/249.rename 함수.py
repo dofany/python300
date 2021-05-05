9c24e674227dc44960109c6124cd60bac06958b1
dofany <kimdohwan17@gmail.com>
2021-05-05 19:08:08 +0900

    249. rename 함수

---
+++ 241_250_파이썬 모듈/249.rename 함수.py
@@ -0,0 +1,4 @@
+# 바탕화면에 텍스트 파일을 하나 생성한 후 os 모듈의 rename 함수를 호출하여 해당 파일의 이름을 변경해보세요.
+
+import os
+os.rename("/Users/dofany/dofany_study/python/basic/data/test.txt","/Users/dofany/dofany_study/python/basic/data/test1.txt")
