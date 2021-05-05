4bf52d1c703885e3bf17e9b4450561e355a3ee65
dofany <kimdohwan17@gmail.com>
2021-05-05 19:37:04 +0900

    260. 에러의 원인

---
+++ 251_260_클래스/260.에러의 원인.py
@@ -0,0 +1,16 @@
+#
+# 아래와 같은 에러가 발생한 원인에 대해 설명하세요.
+#
+# class OMG :
+#     def print() :
+#         print("Oh my god")
+#
+# >>> >>> myStock = OMG()
+# >>> myStock.print()
+# TypeError Traceback (most recent call last)
+# <ipython-input-233-c85c04535b22> in <module>()
+# ----> myStock.print()
+#
+# TypeError: print() takes 0 positional arguments but 1 was given
+
+print("클래스를 호출하는 과정에서 들여쓰기를 했기 때문이다.")
