8d75b57119c47a1d0d28a0d63ceb9840cb11fbfa
dofany <kimdohwan17@gmail.com>
2021-05-05 18:29:51 +0900

    238. 실행결과 예측

---
+++ 231_240_파이썬 함수/238.실행결과 예측.py
@@ -0,0 +1,21 @@
+# 아래 코드의 실행 결과를 예측하라.
+#
+# def 함수1(num) :
+#     return num + 4
+#
+# def 함수2(num) :
+#     return num * 10
+#
+# a = 함수1(10)
+# c = 함수2(a)
+# print(c)
+
+def 함수1(num) :
+    return num + 4
+
+def 함수2(num) :
+    return num * 10
+
+a = 함수1(10)
+c = 함수2(a)
+print(c)
