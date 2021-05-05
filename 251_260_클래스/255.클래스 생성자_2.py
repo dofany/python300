b6380bde176082d4d7043097d121ddf008c2325e
dofany <kimdohwan17@gmail.com>
2021-05-05 19:23:27 +0900

    255. 클래스 생성자_2

---
+++ 251_260_클래스/255.클래스 생성자_2.py
@@ -0,0 +1,12 @@
+# 사람 (Human) 클래스에 (이름, 나이, 성별)을 받는 생성자를 추가하세요.
+#
+# >>> areum = Human("아름", 25, "여자")
+
+class Human:
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+areum = Human("아름",25,"여자")
+print(areum.name)
