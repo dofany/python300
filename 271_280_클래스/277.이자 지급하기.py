ae18e9e1daa83d718d258aca1797f5c12b632c86
dofany <kimdohwan17@gmail.com>
2021-05-06 20:56:00 +0900

    277. 이자 지급하기

---
+++ 271_280_클래스/277.이자 지급하기.py
@@ -0,0 +1,60 @@
+# Account 인스턴스에 저장된 정보를 출력하는 display_info() 메서드를 추가하세요. 잔고는 세자리마다 쉼표를 출력하세요.
+#
+# 은행이름: SC은행
+# 예금주: 파이썬
+# 계좌번호: 111-11-111111
+# 잔고: 10,000원
+
+# Account 클래스에 입금을 위한 deposit 메서드를 추가하세요. 입금은 최소 1원 이상만 가능합니다.
+
+import random
+
+
+class Account:
+    count = 0
+
+    def __init__(self, name, code):
+        self.deposit_count = 0
+        self.name = name
+        self.code = code
+        self.bank = "SC은행"
+
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)
+        num2 = str(num2).zfill(2)
+        num3 = str(num3).zfill(6)
+        self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
+        Account.count += 1
+
+    @classmethod
+    def get_account_num(cls):
+        print(cls.count)  # Account.account_count
+
+    def deposit(self, amount):
+        if amount >= 1:
+            self.code += amount
+            self.deposit_count += 1
+            if self.deposit_count % 5 == 0:         # 5, 10, 15
+                self.code = (self.code * 1.01)
+
+
+    def withdraw(self, amount):
+        if self.code > amount:
+            self.code -= amount
+
+    def display_info(self):
+        print("은행이름: ", self.bank)
+        print("예금주: ", self.name)
+        print("계좌번호: ", self.account_number)
+        print("잔고: ",format(self.code,','))
+
+p = Account("파이썬", 10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(5000)
+p.deposit(5000)
+print(p.code)
